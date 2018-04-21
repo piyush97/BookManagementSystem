a660e24b9957bd6f386a2a2ceb97a9a3b7f4a030
Piyush Mehta <me@piyushmehta.com>
2018-04-21 09:27:05 +0530

    New Changes added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="app" class="container">
+   
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Firebase from 'firebase'
+let config={
+    apiKey: "AIzaSyA7wPioOJ2oFLu31yv9m0KV04MLDJbbae0",
+    authDomain: "bookmanagement-8f49e.firebaseapp.com",
+    databaseURL: "https://bookmanagement-8f49e.firebaseio.com",
+    projectId: "bookmanagement-8f49e",
+    storageBucket: "",
+    messagingSenderId: "135004945659"
+}
+let app=Firebase.initializeApp(config);
+let db=app.database();
+
+let bookRef=db.ref('books');
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  firebase:{
+  books: booRef
   }
 }
 </script>
